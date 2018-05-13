dca9ee5f7ee1d3d50b03d66296c50a1daa6df184
Niels de Waal <nielsdwaal@gmail.com>
2018-05-13 21:39:38 +0200

    Added doxygen header to memory_map

--- ESP/memory_map.hpp
+++ ESP/memory_map.hpp
@@ -1,6 +1,13 @@
 #ifndef MEMORY_MAP_HPP
 #define MEMORY_MAP_HPP
 
+/**
+ * @file        memory_map.hpp
+ * @brief       This header will contain memory addresses, used for controlling the ESP32
+ * @author      Niels de Waal
+ * @license     MIT
+ */
+
 enum BaseAddresses: uint16_t {
     GPIO_FUNC_OUT_SEL_CFG_REG = 0x530
 }
