c43cc0333c1b06dc48ae2b26a31aac6fdfc52f92
Niels de Waal <nielsdwaal@gmail.com>
2018-05-16 21:53:07 +0200

    Fixed SPI gpio pins. SPI object is now correctly created

--- ESP/main/SPI.cpp
+++ ESP/main/SPI.cpp
@@ -4,9 +4,12 @@ SPI::SPI() {
     //Configure handshake line as output
     //gpio_config(&io_conf);
     //Enable pull-ups on SPI lines so we don't detect rogue pulses when no master is connected.
-    gpio_set_pull_mode(pins::GPIO_MOSI, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(pins::GPIO_SCLK, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(pins::GPIO_CS, GPIO_PULLUP_ONLY);
+    //gpio_set_pull_mode(pins::GPIO_MOSI, GPIO_PULLUP_ONLY);
+    //gpio_set_pull_mode(pins::GPIO_SCLK, GPIO_PULLUP_ONLY);
+    //gpio_set_pull_mode(pins::GPIO_CS, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(static_cast<gpio_num_t>(12), GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(static_cast<gpio_num_t>(13), GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(static_cast<gpio_num_t>(14), GPIO_PULLUP_ONLY);
 
     //Initialize SPI slave interface
     ret=spi_slave_initialize(HSPI_HOST, &buscfg, &slvcfg, 1);
