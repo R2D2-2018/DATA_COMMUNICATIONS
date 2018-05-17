133727e9b8c77823c53a11ccb6b7fcda8b21008e
RoxanneMango <roxannevdpol@gmail.com>
2018-05-17 16:08:58 +0200

    Added MemoryBlock enum and Register enum, as well as test cases for both enums

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,9 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "../src/ESP32.hpp"
 
-#include "test_ESP32.cpp"
-#include "test_Dummy_OP_CODES.cpp"
+// VERTICAL_SLICE_1
+#include "test_VS1-ESP32.cpp"
+#include "test_VS1-Dummy_OP_CODES.cpp"
+
+// VERTICAL_SLICE_2
+#include "test_VS2-EnumForRegisters.cpp"
