918feae396635cfb1816fd66d232c61ed0beafd8
RoxanneMango <roxannevdpol@gmail.com>
2018-05-31 22:34:19 +0200

    - Added more comments for constexpressions

--- ESP/main/main.cpp
+++ ESP/main/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "driver/i2c.h"
 
+///< DATA_LEN & DELAY_TIME CONFIGURATION
 constexpr int dataLength = 512; 							///< Data buffer length for test_buffer
 constexpr int rwTestLength = 64; 							///< Data length for r/w test, any value from 0 - dataLength
 constexpr int delayTimeBetweenItemsMS = 1234;				///< Delay time between different test items
@@ -20,12 +21,12 @@ constexpr int masterTransmissionBufferLen = 0;				///< Master transmission buffe
 constexpr int masterReceivingBufferLen = 0;					///< Master receiving buffer size
 constexpr int masterClockFrequency = 100000;				///< Master clock frequency
 
-///<>////
-
+///< SLAVE ADDRESS AND RW BIT CONFIGURATION
 constexpr uint8_t slaveAddress = 0x28;						///< ESP32 slave address
 constexpr i2c_rw_t writeBit = i2c_rw_t::I2C_MASTER_WRITE;	///< Master WRITE bit
 constexpr i2c_rw_t readBit = i2c_rw_t::I2C_MASTER_READ;		///< Master READ bit
 
+///< ACK CONFIGURATION
 constexpr bool checkAck = true;											///< Whether master will check from ACKs from slave
 constexpr i2c_ack_type_t masterAck = i2c_ack_type_t::I2C_MASTER_ACK;	///< Master ACK value
 constexpr i2c_ack_type_t masterNack = i2c_ack_type_t::I2C_MASTER_NACK;	///< Master NACK value (false)
