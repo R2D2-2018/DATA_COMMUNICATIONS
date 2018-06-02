ebc5b0bdec9957289b2c8a0e6fd0c48fce50e042
RoxanneMango <roxannevdpol@gmail.com>
2018-06-02 18:01:13 +0200

    - Directly modified .fetch-build-environment.sh with most recent git changes from build-environment wiki

--- .fetch-build-environment.sh
+++ .fetch-build-environment.sh
@@ -3,7 +3,7 @@
 github_URL="https://github.com/R2D2-2018/Build-environment"
 
 if [ -z ${R2D2_BUILD_ENVIRONMENT} ]; then
-git clone ${github_URL} $1
+git clone ${github_URL} $1 --recursive
 build_environment="$1"
 else
 build_environment=${R2D2_BUILD_ENVIRONMENT}
@@ -12,3 +12,6 @@ fi
 export R2D2_BUILD_ENVIRONMENT=${build_environment}
 
 echo "${build_environment}"
+
+
+
