90e9b223bc466c4270ecb00f6948957b31e5f575
Leidse <kietvanosnabrugge@ziggo.nl>
2018-06-17 16:58:13 +0200

    Added doxygen

--- ESP/I2C_ESP32/main/Acces_buffers.hpp
+++ ESP/I2C_ESP32/main/Acces_buffers.hpp
@@ -4,6 +4,12 @@
 #include "I2C_ESP.hpp"
 #include <iostream>
 
+/**
+ * @file      Acces_buffers.hpp
+ * @brief     This class uses the I2cEsp class to acces the buffers of the master and the slave esp
+ * @author    Kiet van Osnabrugge and Roxanne van der Pol
+ * @license   MIT
+ */
 class AccesBuffers {
   private:
     static constexpr int delayTimeBetweenItemsMS = 1000; ///< Delay time between loop iterations
@@ -11,16 +17,62 @@ class AccesBuffers {
   public:
     AccesBuffers();
 
-    static void getMasterSlaveBuffer(void *taskID);
+    /**
+     * @brief returns the master and slave buffer
+     *
+     * @description
+     *
+     * @return
+     * buffer : uin8_t *
+     */
+    static uint8_t *getMasterSlaveBuffer(void *taskID); // These function if currently empty
+    /**
+     * @brief prints the master and slave buffer with error messages
+     *
+     * @description This function prints the master and the slave buffer. During the process of filling the buffers and printing
+     * them parameters of the esp are checked. And some parameters give information about faulty useage and will give an error
+     * print. There is no clear explanation of the error yet.
+     */
     static void printMasterSlaveBuffer(void *taskID);
 
-    static void getMasterSlaveBuffer(void *taskID);
+    /**
+     * @brief returns the slave buffer
+     *
+     * @description
+     *
+     * @return
+     * buffer : uin8_t *
+     */
+    static uint8_t *getMasterSlaveBuffer(void *taskID); // These function if currently empty
+    /**
+     * @brief prints the slave buffer with error messages
+     *
+     * @description This function prints the slave buffer. During the process of filling the buffers and printing them parameters of
+     * the esp are checked. And some parameters give information about faulty useage and will give an error print. There is no clear
+     * explanation of the error yet.
+     */
     static void printMasterBuffer(void *taskID);
 
-    static void getSlaveBuffer(void *taskID);
+    /**
+     * @brief returns the master buffer
+     *
+     * @description
+     *
+     * @return
+     * buffer : uin8_t *
+     */
+    static uint8_t *getSlaveBuffer(void *taskID); // These function if currently empty
+
+    /**
+     * @brief prints the master buffer with error messages
+     *
+     * @description This function prints the master buffer. During the process of filling the buffers and printing them parameters
+     * of the esp are checked. And some parameters give information about faulty useage and will give an error print. There is no
+     * clear explanation of the error yet.
+     */
     static void printSlaveBuffer(void *taskID);
 
-    static void getDefaultArray(uint8_t *data, int dataLength);
+    static uint8_t *getDefaultArray(uint8_t *data, int dataLength); // These function if currently empty
 };
 
 #endif
