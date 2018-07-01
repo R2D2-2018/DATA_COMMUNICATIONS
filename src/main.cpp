03517f0b183aae885aa8a4210bb70339a0a1ace7
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2018-07-02 00:21:05 +0200

    cleared main

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,5 @@
 
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
-
-    constexpr uint32_t twiSpeed = 400000;
-
-    auto bus = I2C::I2C<0x03>();
-    auto twi = TWI::TWI<twiSpeed>();
-
-    while (true) {
-        auto returns = twi.read<30>(0x28);
-        hwlib::wait_ms(500);
-    }
-
     return 0;
 }
